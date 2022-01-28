62ec5e8f57b2510b6e92b63ec757f1a12d08db70
hazar-nas <hazarcan.nas@gmail.com>
2022-01-28 19:47:31 +0300

    finished

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Comment(props) {
-  return <div className="comment">{props.commentText}</div>;
+  return (
+    <div className="comment">
+      {props.commentText}
+    </div>
+  )
+
 }
 
 export default Comment;
